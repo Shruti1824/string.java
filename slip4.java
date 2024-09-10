ad0c832af679a81aa035636fa630cc97798b046b
Shruti varma <156296935+Shruti1824@users.noreply.github.com>
2024-09-10 03:31:24 -0700

    Add files via upload
    string in python to print hello

---
+++ slip4.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class slip4{
+public static void main(String[] arg)
+{
+String s="Hello World!";
+for(int i=0;i<s.length();i+=2)
+{
+System.out.println(s.charAt(i));
+}
+}
+}
